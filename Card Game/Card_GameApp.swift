826f59743e3e482fd97c008ed33f98d617092e56
Mark Satterfield <mark@parmasdev.com>
2023-03-16 19:58:59 -0600

    Initial Commit

---
+++ Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by Mark on 2023-03-16.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
